0d323f186ab54c23224a89115456d5503f9affc1
jorgeastorga <jeastorga@gmail.com>
2012-12-02 23:51:57 -0800

    added files to try having two different view controllers

---
+++ NIBViewController.h
@@ -0,0 +1,13 @@
+//
+//  NIBViewController.h
+//  SinglePlatformTest
+//
+//  Created by Jorge E. Astorga on 12/2/12.
+//  Copyright (c) 2012 Test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NIBViewController : UIViewController
+
+@end
---
+++ NIBViewController.m
@@ -0,0 +1,38 @@
+//
+//  NIBViewController.m
+//  SinglePlatformTest
+//
+//  Created by Jorge E. Astorga on 12/2/12.
+//  Copyright (c) 2012 Test. All rights reserved.
+//
+
+#import "NIBViewController.h"
+
+@interface NIBViewController ()
+
+@end
+
+@implementation NIBViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
