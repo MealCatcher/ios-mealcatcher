0de8dcda51390f76ab3686c6e765a9b22bba8c4a
jorgeastorga <jeastorga@gmail.com>
2013-07-21 20:29:46 -0700

    Adding the Favorite model

---
+++ MealCatcher/Model/Favorite.h
@@ -0,0 +1,19 @@
+//
+//  Favorite.h
+//  MealCatcher
+//
+//  Created by Jorge E. Astorga on 7/21/13.
+//  Copyright (c) 2013 Test. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Favorite : NSObject
+
+@property (strong, nonatomic) NSString *name;
+//@property (nonatomic, strong) NSString *address;
+//@property (nonatomic, strong) NSString *id;
+//@property (nonatomic) NSUInteger *rating;
+//@property (nonatomic, strong) NSString *reference;
+
+@end
---
+++ MealCatcher/Model/Favorite.m
@@ -0,0 +1,18 @@
+//
+//  Favorite.m
+//  MealCatcher
+//
+//  Created by Jorge E. Astorga on 7/21/13.
+//  Copyright (c) 2013 Test. All rights reserved.
+//
+
+#import "Favorite.h"
+
+@interface Favorite() : NSObject
+
+@end
+
+
+@implementation Favorite
+
+@end
